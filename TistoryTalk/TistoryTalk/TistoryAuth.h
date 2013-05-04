ea6793889b4bc1f1ba5ad269d35c95ef5b9ca0c6
AhnSeongHyun <sh84.ahn@gmail.com>
2013-05-04 09:21:01 +0900

    [수정] 인증뷰, 각종 폐기 뷰 컨트롤러
    글쓰기, 설정, 내블로그를 제외한 모든 뷰 컨트롤러를 제외
    설정에서 인증뷰가 동작하도록 수정

---
+++ TistoryTalk/TistoryTalk/TistoryAuth.h
@@ -0,0 +1,13 @@
+//
+//  TistoryAuth.h
+//  TistoryTalk
+//
+//  Created by an seonghyun on 13. 5. 3..
+//  Copyright (c) 2013년 an seonghyun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TistoryAuth : NSObject
+
+@end
